97b7f2e3c701df746ae4e326731e402bf9e960ed
Stiven Yepez <stiven.yepez@raddarstudios.com>
2022-05-10 23:48:53 -0500

    clase 10/05/2022

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,36 @@
+
+
+    function getMovieById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    function getAllActorsDataByMovieId( movieId: string ) {
+        console.log({movieId});
+    }
+
+    function getBioActorById( actorId: string ) {
+        console.log({actorId });
+    }
+    interface MovieData{
+        cast:        string[]
+        description: string, 
+        rating:      number,
+        title:       string,
+    }
+    function createMovie({title, description, rating, cast} : MovieData) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
